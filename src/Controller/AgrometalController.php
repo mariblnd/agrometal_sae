d8617f84c005efc46fb40328bd1a83f9d0c650e2
mariblnd <marieblanchard7@laposte.net>
2024-03-08 14:58:26 +0100

    Routs and base of controllers

---
+++ src/Controller/AgrometalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AgrometalController extends AbstractController
+{
+    public function homepageController(){}
+
+    public function referencesController(){}
+
+    public function equipementsController(){}
+
+    public function adminpanelController(){}
+
+
+}
+
+?>
